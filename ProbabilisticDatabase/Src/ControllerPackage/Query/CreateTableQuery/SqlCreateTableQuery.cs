3cd3d04c76770dab8bdae6db656a0751eea48455
Yan <yanbzybzy2@gmail.com>
2013-05-25 16:16:22 +0100

    adding create table SQL support
    create table partial implemented, seems unnecessary

--- ProbabilisticDatabase/Src/ControllerPackage/Query/CreateTableQuery/SqlCreateTableQuery.cs
+++ ProbabilisticDatabase/Src/ControllerPackage/Query/CreateTableQuery/SqlCreateTableQuery.cs
@@ -9,23 +9,72 @@ namespace ProbabilisticDatabase.Src.ControllerPackage.Query.CreateTableQuery
 {
     public class SqlCreateTableQuery
     {
-        private string sql;
-        private string tableName;
+        private string _sql;
+        private string _tableName;
+
+        public string TableName
+        {
+            get { return _tableName; }
+        }
+        private List<String> _attributeNames;
+
+        public List<String> AttributeNames
+        {
+            get { return _attributeNames; }
+        }
+        private List<String> _attributeTypes;
+
+        public List<String> AttributeTypes
+        {
+            get { return _attributeTypes; }
+        }
 
         public SqlCreateTableQuery(string sql)
         {
-            this.sql = sql;
+            this._sql = sql;
             processAndPopulateEachField();
         }
 
-        internal void processAndPopulateEachField()
+        private void processAndPopulateEachField()
         {
             // pattern here is CREATE TABLE tablename ( columnName columnType, ... repeat )
-            string sPattern = @"CREATE\s+TABLE\s+(?<tableName>\w+)\s+((?<valueClause>.*))";
-            Match match = Regex.Match(this.sql, sPattern, RegexOptions.IgnoreCase);
-            //match.Captures.Equals
+            string sPattern = @"CREATE\s+TABLE\s+(?<tableName>\w+)\s+\((?<valueClause>.+)\)";
+            Match match = Regex.Match(this._sql, sPattern, RegexOptions.IgnoreCase);
+
+            if (match.Success)
+            {
+                _tableName = match.Groups["tableName"].Value;
+                var attributeClause = match.Groups["valueClause"].Value;
+                processAttributeValue(attributeClause,out _attributeNames,out _attributeTypes);
+            }
+            else
+            {
+                _tableName = "";
+
+            }
+            
 
         }
+
+        private void processAttributeValue(string attributeClause, out List<string> _attributeNames, out List<string> _attributeTypes)
+        {
+            // pattern here is  columnName columnType, ... repeat 
+            string sPattern = @"(?<attributeName>\w+)\s+(?<attributeType>.+?)\s*(,|\z)";
+            MatchCollection matches = Regex.Matches(attributeClause, sPattern, RegexOptions.IgnoreCase);
+            _attributeNames = new List<string>();
+            _attributeTypes = new List<string>();
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                var current = matches[i];
+                var name = current.Groups["attributeName"].Value;
+                var type = current.Groups["attributeType"].Value;
+                _attributeNames.Add(name);
+                _attributeTypes.Add(type);
+            }
+
+        }
+
 
 
 
---
+++ ProbabilisticDatabase/Src/ControllerPackage/QueryHandler/CreateTableHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticDatabase.Src.ControllerPackage.QueryHandler
+{
+    public class CreateTableHandler
+    {
+        private Query.CreateTableQuery.SqlCreateTableQuery cquery;
+        private IStandardDatabase underlineDatabase;
+
+        public CreateTableHandler(Query.CreateTableQuery.SqlCreateTableQuery cquery, IStandardDatabase underlineDatabase)
+        {
+            this.cquery = cquery;
+            this.underlineDatabase = underlineDatabase;
+        }
+
+        internal System.Data.DataTable HandleCreateTableQuery()
+        {
+            createAttributeTables(cquery.TableName,cquery.AttributeNames,cquery.AttributeTypes,underlineDatabase);
+            createPossibleStatesTable(cquery.TableName, cquery.AttributeNames, cquery.AttributeTypes, underlineDatabase);
+            createPossibleWorldsTable(cquery.TableName, cquery.AttributeNames, cquery.AttributeTypes, underlineDatabase);
+            return null;
+        }
+
+
+
+        private void createPossibleStatesTable(string tableName, List<string> attNames, List<string> attTypes, IStandardDatabase underlineDatabase)
+        {
+            String[] attributeNames = { "var", "v" };
+            String[] attributeTypes = { "INT", "INT" };
+            string attributeTableName = tableName + "_PossibleStates";
+
+            List<String> attributeNamesList = attributeNames.ToList();
+            List<String> attributeTypesList = attributeTypes.ToList();
+
+            attributeNamesList.AddRange(attNames);
+            attributeTypesList.AddRange(attTypes);
+
+            // p is the last attribute
+            attributeNamesList.Add("p");
+            attributeTypesList.Add("float");
+
+            underlineDatabase.CreateNewTable(attributeTableName, attributeNamesList.ToArray(), attributeTypesList.ToArray());
+        }
+
+        private void createPossibleWorldsTable(string tableName, List<string> attNames, List<string> attTypes, IStandardDatabase underlineDatabase)
+        {
+            String[] attributeNames = { "worldNo" };
+            String[] attributeTypes = { "INT" };
+            string attributeTableName = tableName + "_PossibleWorlds";
+
+            List<String> attributeNamesList = attributeNames.ToList();
+            List<String> attributeTypesList = attributeTypes.ToList();
+
+            attributeNamesList.AddRange(attNames);
+            attributeTypesList.AddRange(attTypes);
+
+            // p is the last attribute
+            attributeNamesList.Add("p");
+            attributeTypesList.Add("float");
+
+            underlineDatabase.CreateNewTable(attributeTableName, attributeNamesList.ToArray(), attributeTypesList.ToArray());
+        }
+
+        private void createAttributeTables(string tableName, List<string> attNames, List<string> attTypes, IStandardDatabase underlineDatabase)
+        {
+            String[] attributeNames1 = { "var", "v", "TupleExistence", "p" };
+            String[] attributeTypes1 = { "INT", "INT", "NVARCHAR(MAX)", "float" };
+            underlineDatabase.CreateNewTable(tableName + "_0", attributeNames1, attributeTypes1);
+
+            for (int i = 1; i <= attNames.Count; i++)
+            {
+                string attributeTableName = tableName + "_" + i;
+                String[] attributeNames = { "var", "v", attNames[i-1], "p" };
+                String[] attributeTypes = { "INT", "INT", attTypes[i-1], "float" };
+                underlineDatabase.CreateNewTable(attributeTableName, attributeNames, attributeTypes);
+            }
+        }
+
+
+    }
+}
